3a0a0e4592477942b571499f23e1bd782e54bd4e
zhime <931013000@qq.com>
2024-09-14 14:01:36 +0800

    add gin

--- main.go
+++ main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
-	"github.com/zhime/monitor/core"
-	"github.com/zhime/monitor/database"
-	"github.com/zhime/monitor/global"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
-	global.VIPER = core.Viper()
-	global.DB = core.InitMysql()
+	//global.VIPER = core.Viper()
+	//global.DB = core.InitMysql()
 
-	global.WG.Add(1)
-	go database.CheckSlaveStatus()
-	global.WG.Wait()
+	//global.WG.Add(1)
+	//go database.CheckSlaveStatus()
+	//global.WG.Wait()
+
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"data": "ok",
+		})
+	})
+	if err := r.Run(); err != nil {
+		panic(err)
+	}
 
 }
