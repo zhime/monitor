daa7539263530715db664eb228187a75fb9d936f
zhime <931013000@qq.com>
2024-09-18 15:45:53 +0800

    update

--- model/user.go
+++ model/user.go
@@ -1,12 +1,14 @@
 package model
 
+import "github.com/zhime/monitor/global"
+
 type User struct {
-	ID       uint   `gorm:"primaryKey"`
+	global.MODEL
 	Name     string `gorm:"size:100"`
 	Email    string `gorm:"size:100;unique"`
 	Password string `gorm:"size:255"`
 }
 
-func (u User) TableName() string {
+func (User) TableName() string {
 	return "user"
 }
